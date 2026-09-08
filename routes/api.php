69f7b5b788e29680eb70d36b709393dd59fd8ad9
Melvin <melvin092007@gmail.com>
2026-09-08 10:27:42 -0600

    feat:Rutas de comanda+importacion controlador comandasa

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\ComandaController;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
@@ -13,3 +14,10 @@ Route::post('/categories', [CategoriaController::class, 'store']);
 Route::get('/categories/{category}', [CategoriaController::class, 'show']);
 Route::put('/categories/{category}', [CategoriaController::class, 'update']);
 Route::delete('/categories/{category}', [CategoriaController::class, 'destroy']);
+
+//Comanda rutas
+Route::get('/comandas', [ComandaController::class, 'index']);
+Route::post('/comandas', [ComandaController::class, 'store']);
+Route::get('/comandas/{comanda}', [ComandaController::class, 'show']);
+Route::put('/comandas/{comanda}', [ComandaController::class, 'update']);
+Route::delete('/comandas/{comanda}', [ComandaController::class, 'destroy']);
