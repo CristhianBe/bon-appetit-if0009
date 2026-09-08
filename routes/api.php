4d8f2daba5eb4f6a43aa6db3a682d816bcbb119b
Melvin <melvin092007@gmail.com>
2026-09-08 10:21:39 -0600

    feat:Rutas de categorias+importacion controlador categoria

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\CategoriaController;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 //Categoria rutas 
+Route::get('/categories', [CategoriaController::class, 'index']);
+Route::post('/categories', [CategoriaController::class, 'store']);
+Route::get('/categories/{category}', [CategoriaController::class, 'show']);
+Route::put('/categories/{category}', [CategoriaController::class, 'update']);
+Route::delete('/categories/{category}', [CategoriaController::class, 'destroy']);
